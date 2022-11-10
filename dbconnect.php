b9ee363464e96011a88a6e2ffe41505ea562da0a
ZairoAngelo <yuuyakizami48@gmail.com>
2022-11-10 22:08:50 +0800

    new

--- dbconnect.php
+++ dbconnect.php
@@ -1,42 +1,23 @@
+
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-// echo "Connected successfully";
-
-// Create database
-// $sql = "CREATE DATABASE Test";
-// if ($conn->query($sql) === TRUE) {
-//   echo "Database created successfully";
-// } else {
-//   echo "Error creating database: " . $conn->error;
-// }
-
-
-//Create Table for User Message
-// $sql = "CREATE TABLE UserMessage(
-//   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//   email VARCHAR(50) NOT NULL,
-//   message VARCHAR(500) NOT NULL,
-//   reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-// )";
-
-// if($conn->query($sql) === TRUE){
-//   echo "Table UserMessage created successfully";
-// } else {
-//   echo "Error creating table: " . $conn->error;
-// }
-
-$conn->close();
-?>
-
-
+ 
+ $conn = "";
+   
+ try {
+     $servername = "localhost:3306";
+     $dbname = "test";
+     $username = "root";
+     $password = "";
+   
+     $conn = new PDO(
+         "mysql:host=$servername; dbname=test",
+         $username, $password);
+      
+    $conn->setAttribute(PDO::ATTR_ERRMODE,
+                     PDO::ERRMODE_EXCEPTION);
+ }
+ catch(PDOException $e) {
+     echo "Connection failed: " . $e->getMessage();
+ }
+  
+ ?>
--- login.php
+++ login.php
@@ -1,26 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<title>MySqlEdu - Login</title>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <body>
+<nav class="navbar navbar-expand-lg" role="navigation">
+
+<div class="container-fluid" id="navbarSupportedContent">
+    <a class="navbar-brand" href="index.php">MySqlEdu</a>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="contact.php">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="login.php" aria-current="page">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="register.php">Register</a>
+            </li>
+</ul>
+    </div>
+</div>
+</nav>
+
+
 <article class="container-fluid">
         <h1 class="text-center mb-5">Login</h1>
-        <form class="container" method="post" action="message.php">
+        <form class="container" method="post" action="validate.php">
             <div class="mx-auto w-25 form-floating mb-3"> <!-- mb = adding margin bottom-->
-                <input type="username" name="username" class="form-control" id="floatingInput" aria-describedby="emailHelp">
-                <label for="floatingInput" class="form-label" placeholder="name@example.com">Email Address:</label>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <input type="username" name="username" value="" class="form-control" id="floatingInput" aria-describedby="emailHelp">
+                <label for="floatingInput" class="form-label" placeholder="name@example.com">Username</label>
             </div>
-            <div class="mx-auto w-25 mb-3 input-group">
-                <label for="Message1"  class="input-group-text">Message:</label>
-                <textarea  class="form-control" name="message" id="Message1" placeholder="Leave a comment here"></textarea>
+            <div class="mx-auto w-25 form-floating mb-3"> <!-- mb = adding margin bottom-->
+                <input type="password" name="password" value="" class="form-control" id="floatingInput" aria-describedby="emailHelp">
+                <label for="floatingInput" class="form-label">Password</label>
             </div>
-            <div class="d-flex justify-content-center"> <button type="submit" class="btn btn-primary">Submit</button></div>
-          
+            <div class="d-flex justify-content-center"> 
+                <button type="submit" class="btn btn-primary" name="login" value="Sign In">Login</button>
+            </div>
         </form>
     </article>
 </body>
