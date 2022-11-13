f95ca8c03f59a6e3c8cbe9b26c110a2a069f4ff2
ZairoAngelo <yuuyakizami48@gmail.com>
2022-11-14 00:31:07 +0800

    process

--- process-register-page.php
+++ process-register-page.php
@@ -3,20 +3,21 @@
 // Check that form has been submitted:
         $errors = array(); // Initialize an error array.                                #1
        // Check for a first name:                                                       #2
-       $name = trim($_POST['name']);
-       if (empty($name)) {
-              $errors[] = 'You forgot to enter your name.';
-       }
-                // Check for a last name:
-       $age = trim($_POST['age']);
-       if (empty($age)) {
-              $errors[] = 'You forgot to enter your age.';
-       }
-       // Check for an email address:
-       $email = trim($_POST['email']);
-       if (empty($email)) {
-              $errors[] = 'You forgot to enter your email address.';
-       }
+      $first_name = filter_var($_POST['first_name'], FILTER_UNSAFE_RAW);
+      if(empty($first_name)){
+       $errors[] = 'You Forgot to enter your First Name';
+      }
+      // Check for a last name:                                                     
+      $last_name = filter_var($_POST['last_name'], FILTER_UNSAFE_RAW);
+      if(empty($last_name)){
+       $errors[] = 'You Forgot to enter your Last Name';
+      }
+       // Check for an email address:                                       #2
+       $email = filter_var( $_POST['email'], FILTER_SANITIZE_EMAIL);
+        if  ((empty($email)) || (!filter_var($email, FILTER_VALIDATE_EMAIL))) {
+               $errors[] = 'You forgot to enter your email address';
+               $errors[] = ' or the e-mail format is incorrect.';
+        }
        // Check for a password and match against the confirmed password:               #3
        $password1 = trim($_POST['password1']);
        $password2 = trim($_POST['password2']);
@@ -33,13 +34,13 @@ try {
             $hashed_passcode = password_hash($password1, PASSWORD_DEFAULT);           //#5
              require ('mysqli_connect.php'); // Connect to the db.                    //#6
         // Make the query:                                                              #7
-        $query = "INSERT INTO users (userid, name, age, email, password, registration_date) ";
+        $query = "INSERT INTO users (user_id, first_name, last_name, email, password, registration_date) ";
                 $query .="VALUES(' ', ?, ?, ?, ?, NOW() )";                           //#8
         $q = mysqli_stmt_init($dbcon);                                                //#9
         mysqli_stmt_prepare($q, $query);
         // use prepared statement to ensure that only text is inserted
         // bind fields to SQL Statement
-        mysqli_stmt_bind_param($q, 'ssss', $name, $age, $email, $hashed_passcode);
+        mysqli_stmt_bind_param($q, 'ssss', $first_name, $last_name, $email, $hashed_passcode);
      // execute query
         mysqli_stmt_execute($q);
         if (mysqli_stmt_affected_rows($q) == 1) { // One record inserted               #10
@@ -54,11 +55,11 @@ try {
                 // Debugging message below do not use in production
                 //echo '<p>' . mysqli_error($dbcon) . '<br><br>Query: ' . $query . '</p>';
                                mysqli_close($dbcon); // Close the database connection.
-                // include footer then close program to stop execution
-                        // echo '<footer class="jumbotron text-center col-sm-12"
-                        //                style="padding-bottom:1px; padding-top:8px;">
-                        //                        include("footer.php");
-                        //                       </footer>';
+                // include footers then close program to stop execution
+                     //    echo '<footer class="jumbotron text-center col-sm-12"
+                     //                   style="padding-bottom:1px; padding-top:8px;">
+                     //                           include("footer.php");
+                     //                          </footer>';
                              exit();
        }
    }
@@ -80,4 +81,3 @@ try {
                $errorstring .= "Please try again.<br>";
                echo "<p class=' text-center col-sm-2' style='color:red'>$errorstring</p>";
                }// End of if (empty($errors)) IF.
-?>
